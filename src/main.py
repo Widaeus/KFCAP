aca8dd6ca84579fed17c668e386b97c05cdcc7fe
Jacob Widaeus <jacob.widaeus@gmail.com>
2024-10-18 10:39:07 +0200

    PyCap module works, need to set environ to api_token

--- src/main.py
+++ src/main.py
@@ -1,19 +1,22 @@
 import os
-from reader_prep import reader_prep_csv
-from utils import export_redcap_data, export_redcap_info
-import getpass
 import pandas as pd
-from redcap.methods import records
+from redcap.project import Project
+
+# Define the API token as environment variable as
+# set REDCAP_API_TOKEN="api token"
 
 # Define the URL and API token for your REDCap project
-REDCAP_API_URL = 'https://redcap.example.com/api/'
-API_TOKEN = 'your_api_token'
+REDCAP_API_URL = 'https://redcap.ki.se/api/'
+API_TOKEN = os.getenv('REDCAP_API_TOKEN_mind')
+
+if not API_TOKEN:
+    raise ValueError("API token not found. Please set the REDCAP_API_TOKEN environment variable.")
 
 # Initialize the REDCap project
 project = Project(REDCAP_API_URL, API_TOKEN)
 
 # Export records
-records = project.export_records(format='json')
+records = project.export_records(format_type='json')
 
 # Convert the exported records to a DataFrame
 df = pd.DataFrame(records)
