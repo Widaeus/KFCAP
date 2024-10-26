521afbc9f01c3fbebea1f3ca9530c6fca2040396
Widaeus <jacob.widaeus@gmail.com>
2024-10-26 12:55:26 +0200

    changed name on .exe

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pyinstaller --onefile --clean --name="KFC_REDcap_import" --icon=icons/science.ico --noconsole --version-file=pyinstaller_version.txt src/main.py
+pyinstaller --onefile --clean --name="KFCAP" --icon=icons/science.ico --noconsole --version-file=pyinstaller_version.txt src/main.py
